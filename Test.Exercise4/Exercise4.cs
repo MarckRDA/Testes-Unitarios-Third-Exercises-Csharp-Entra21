98c3828f2e7db09a1f636cca6e6b221a84691b2c
MarckRDA <marcos.edo132@gmail.com>
2020-09-18 15:13:18 -0300

    Code refactored

--- Test.Exercise4/Exercise4.cs
+++ Test.Exercise4/Exercise4.cs
@@ -1,18 +1,11 @@
 using System;
+using System.Linq;
 
 namespace Test.Exercise4
 {
     public class Exercise4
     {
-        public double SumBetweenAAndBArray(double[] numbers)
-        {
-            var sum = 0.0;
-            for (int i = 0; i < numbers.Length; i++)
-            {                
-                sum = (numbers[i] % 2 != 0) ? sum += numbers[i] : sum;
-            }
-
-            return sum;
-        }
+        public double SumBetweenAAndBArray(double[] numbers) => Math.Round(numbers.ToList().Where(x => x % 2 != 0).Sum(), 2);
+        
     }
 }
--- Test.Exercise4/TestExercise4.cs
+++ Test.Exercise4/TestExercise4.cs
@@ -5,21 +5,20 @@ namespace Test.Exercise4
 {
     public class TestExercise4
     {
-        [Fact]
-        public void Should_Return_A_Sum_Of_Elements_What_Are_Odds()
+        [Theory]
+        [InlineData(new double[]{2, 45, 3, 7, 6}, 55.00)]
+        [InlineData(new double[]{-5, -6, 89.43, 83, 0, -8, 54}, 167.43)]
+        [InlineData(new double []{12, 4.3, 6.5, 15.43, -94}, 26.23)]
+        public void Should_Return_A_Sum_Of_Elements_What_Are_Odds(double[] numbers, double expectedResult)
         {
             //Given
             var exercise4 = new Exercise4();
-            var numbers = new double[]
-            {
-                2,45,3,7,6
-            };
-
+            
             //When
             var result = exercise4.SumBetweenAAndBArray(numbers);
 
             //Then
-            Assert.Equal(55, result);
+            Assert.Equal(expectedResult, result);
         }
     }
 }
