229fa535d2477562933a565d29c9f91611107133
MarckRDA <marcos.edo132@gmail.com>
2020-09-18 15:49:03 -0300

    Code refactored

--- Test.Exercise6/Exercise6.cs
+++ Test.Exercise6/Exercise6.cs
@@ -1,31 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Test.Exercise6
 {
     public class Exercise6
     {
-        public (double biggestNumber, int position) BiggestNumberInArray(double[] numbers)
+        public (double biggestNumber, int position) BiggestNumberInArray(List<double> numbers)
         {
-            (double value, int position, int flag) biggestNumber = (0.0, 0, 0);
+            (double value, int position) biggestNumber = (0.0, 0);
             
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = 0; j < numbers.Length; j++)
-                {
-                    if (numbers[i] > numbers[j])
-                    {
-                        biggestNumber.value = numbers[i];
-                        biggestNumber.position = i;
-                        biggestNumber.flag++;
-                    }
-                }
-                if (biggestNumber.flag == numbers.Length - 1)
-                {
-                    break;
-                }
-                biggestNumber.flag = 0;
-            }
-
+            biggestNumber.value = numbers.Max();
+            biggestNumber.position = numbers.IndexOf(biggestNumber.value);
+            
             return (biggestNumber.value, biggestNumber.position);
         }
     }
--- Test.Exercise6/TestExercise6.cs
+++ Test.Exercise6/TestExercise6.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Test.Exercise6
@@ -10,9 +11,9 @@ namespace Test.Exercise6
         {
             //Given
             var exercise6 = new Exercise6();
-            var numbers = new double[]
+            var numbers = new List<double>()
             {
-                1,4,23,5,76,8,9,1,2,0
+                1.09, 4.2, -23, 5, 76, 8, -9, 1, 2.43, 0
             };
 
             //When
