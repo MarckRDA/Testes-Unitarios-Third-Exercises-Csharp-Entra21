b6a462fca530ae1c748660da63c6368add837e6c
MarckRDA <marcos.edo132@gmail.com>
2020-09-18 16:33:06 -0300

    Code refactored

--- Test.Exercise8/Exercise8.cs
+++ Test.Exercise8/Exercise8.cs
@@ -1,22 +1,14 @@
 using System;
+using System.Linq;
 
 namespace Test.Exercise8
 {
     public class Exercise8
     {
-        public int VogalVectorCount(string[] letters)
-        {
-            var countVogal = 0;
-
-            for (int i = 0; i < letters.Length; i++)
-            {
-                if (letters[i] == "a" || letters[i] == "e" || letters[i] == "i" || letters[i] == "o" || letters[i] == "u")
-                {
-                    countVogal++;
-                }
-            }
-            
-            return countVogal;
-        }
+        public int VogalVectorCount(char[] letters) => 
+                                                letters.ToList()
+                                                .Where(n =>  n == 'a' || n == 'e' || n == 'i' || n == 'o' || n == 'u')   
+                                                .Count();
+        
     }
 }
--- Test.Exercise8/TestExercise8.cs
+++ Test.Exercise8/TestExercise8.cs
@@ -6,11 +6,11 @@ namespace Test.Exercise8
     public class TestExercise8
     {
         [Theory]
-        [InlineData(new string[]{"e", "s", "d", "a", "g", "i", "o", "h", "l", "w"}, 4)]
-        [InlineData(new string[]{"q", "f", "u", "i", "s", "b", "p", "k", "h", "a"}, 3)]
-        [InlineData(new string[]{"t", "i", "s", "i", "g", "i", "e", "u", "g", "a"}, 6)]
-        [InlineData(new string[]{"i", "o", "e", "a", "u", "k", "o", "i", "l", "w"}, 7)]
-        public void Should_Return_How_Many_Vogals_There_Are_In_Array(string[] letters, int expectedResult)
+        [InlineData(new char[]{'e', 's', 'd', 'a', 'g', 'i', 'o', 'h', 'l', 'w'}, 4)]
+        [InlineData(new char[]{'q', 'f', 'u', 'i', 's', 'b', 'p', 'k', 'h', 'a'}, 3)]
+        [InlineData(new char[]{'t', 'i', 's', 'i', 'g', 'i', 'e', 'u', 'g', 'a'}, 6)]
+        [InlineData(new char[]{'i', 'o', 'e', 'a', 'u', 'k', 'o', 'i', 'l', 'w'}, 7)]
+        public void Should_Return_How_Many_Vogals_There_Are_In_Array(char[] letters, int expectedResult)
         {
             //Given
             var exercise8 = new Exercise8();
